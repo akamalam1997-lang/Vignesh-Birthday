35eba04629df4d594cdc1cbdde0175eced28619d
akamalam1997-lang <akamalam1997@gmail.com>
2026-07-01 17:47:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+/* ==========================================
+Loading Screen
+========================================== */
+
+function showLoading(){
+
+    const loading=document.getElementById("loading");
+
+    if(!loading) return;
+
+    let percent=0;
+
+    const text=document.getElementById("loadingText");
+
+    const bar=document.getElementById("loadingBar");
+
+    const timer=setInterval(()=>{
+
+        percent++;
+
+        if(text)
+            text.innerHTML=percent+"%";
+
+        if(bar)
+            bar.style.width=percent+"%";
+
+        if(percent>=100){
+
+            clearInterval(timer);
+
+            setTimeout(()=>{
+               hideScene("loading");
+                showScene("welcome");
+startBirthdayFireworks();
+                startTyping();
+
+            },600);
+
+        }
+
+    },30);
+
+}
